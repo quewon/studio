d67caecc494cd3377838d6dadb01de4720d7704e
quewon <36914642+quewon@users.noreply.github.com>
2023-07-18 20:34:47 +0900

    file reading

--- js/engine.js
+++ js/engine.js
@@ -526,58 +526,117 @@ function exportBaked() {
   return JSON.stringify(data, null, 2);
 }
 
-function importProject(json) {
+function importJSON(json) {
   var data = JSON.parse(json);
-
-  if (!('isProject' in data)) {
-    alert("this is not a project file!");
-    return;
-  }
-
-  initSettings(data.SETTINGS);
-
-  for (let t of data.TRACKS) {
-    var track = new Track();
-    track.setTotalTime(t.totalTime);
-    for (let c of t.clips) {
-      var clip = new Clip(track);
-      for (let e of c.log) {
-        new RecordedEvent(e.strippedEvent, clip, e.localTimeStamp);
-      }
-      clip.startTime = c.startTime;
-      clip.totalTime = c.totalTime;
-      clip.trimStart = c.trimStart;
-      clip.setTrimmedTime(c.trimmedTime);
-    }
-    if (t.selected) track.select();
-    if (t.locked) track.lock();
-
-    setPlayheadTime(t.totalTime);
+  if (data.isProject) {
+    importProject(data);
+  } else if (data.isBaked) {
+    importBaked(data);
+  } else {
+    alert("the file you're trying to load is not a project or a recording.");
   }
 }
 
-function importBaked(json) {
-  var data = JSON.parse(json);
+function importProject(data) {
+  try {
+    if (data.SETTINGS) initSettings(data.SETTINGS);
 
-  if (!('isBaked' in data)) {
-    alert("this is not a recording file!");
-    return;
-  }
+    if (data.TRACKS) {
+      for (let t of data.TRACKS) {
+        var track = new Track();
+        track.setTotalTime(t.totalTime);
+        for (let c of t.clips) {
+          var clip = new Clip(track);
+          for (let e of c.log) {
+            new RecordedEvent(e.strippedEvent, clip, e.localTimeStamp);
+          }
+          clip.startTime = c.startTime;
+          clip.totalTime = c.totalTime;
+          clip.trimStart = c.trimStart;
+          clip.setTrimmedTime(c.trimmedTime);
+        }
+        if (t.selected) track.select();
+        if (t.locked) track.lock();
 
-  initSettings(data.SETTINGS);
-
-  for (let t of data.TRACKS) {
-    var track = new Track();
-    if (t.length > 0) {
-      var clip = new Clip(track);
-      for (let inputEvent of t) {
-        new RecordedEvent(inputEvent.strippedEvent, clip, inputEvent.timeStamp);
+        setPlayheadTime(t.totalTime);
       }
-      clip.updateLogElement();
-      clip.totalTime = t[t.length - 1].timeStamp;
-      clip.setStartTime(0);
-      clip.setTrimmedTime(clip.totalTime);
     }
-    setPlayheadTime(track.totalTime);
+  } catch {
+    alert("file could not be fully loaded...");
+  }
+}
+
+function importBaked(data) {
+  try {
+    if (data.SETTINGS) initSettings(data.SETTINGS);
+
+    if (data.TRACKS) {
+      for (let t of data.TRACKS) {
+        var track = new Track();
+        if (t.length > 0) {
+          var clip = new Clip(track);
+          for (let inputEvent of t) {
+            new RecordedEvent(inputEvent.strippedEvent, clip, inputEvent.timeStamp);
+          }
+          clip.updateLogElement();
+          clip.totalTime = t[t.length - 1].timeStamp;
+          clip.setStartTime(0);
+          clip.setTrimmedTime(clip.totalTime);
+        }
+        setPlayheadTime(track.totalTime);
+      }
+    }
+  } catch {
+    alert("file could not be fully loaded...");
+  }
+}
+
+function getDateString() {
+  var date = new Date();
+
+  var d = " ";
+
+  var day = date.getFullYear() +d+ date.getMonth() +d+ date.getDate();
+
+  var hour = date.getHours();
+  if (hour < 10) hour = "0"+hour;
+
+  var min = date.getMinutes();
+  if (min < 10) min = "0"+min;
+
+  var sec = date.getSeconds();
+  if (sec < 10) sec = "0"+sec;
+
+  var time = hour +d+ min +d+ sec;
+
+  return day +d+d+ time;
+}
+
+function createProjectFile(name) {
+  name = prompt("what will you name this project?", getDateString());
+  if (name) downloadFile([exportProject()], name+".txtstudio");
+}
+
+function createRecordingFile(name) {
+  name = prompt("what will you name this recording?", getDateString());
+  if (name) downloadFile([exportBaked()], name+".txtrec");
+}
+
+function downloadFile(json, fileName) {
+  const file = new Blob(json, { type: 'text/plain' });
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(file);
+  link.download = fileName;
+  link.click();
+  URL.revokeObjectURL(link.href);
+}
+
+var fileReader = new FileReader();
+function readFile(input) {
+  if (input.files.length > 0) {
+    fileReader.readAsText(input.files[0]);
+    fileReader.onload = function(e) {
+      importJSON(e.target.result);
+    }
   }
 }
